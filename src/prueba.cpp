710aa8a4685f9facfa455b55226a404528e95e9f
Carlos Duelo <carlos.duelo@gmail.com>
2015-01-18 13:04:59 +0100

    Parse XML file

--- src/prueba.cpp
+++ src/prueba.cpp
@@ -1,6 +1,8 @@
 #include <tinyxml.h>
 #include <iostream>
 
+#include <string>
+
 int main(int argc, char ** argv)
 {
     if( argc != 2 )
@@ -16,13 +18,65 @@ int main(int argc, char ** argv)
         return 0;
     }
 
-    const TiXmlElement* root = doc.RootElement();
-
+    const TiXmlElement* root = doc.FirstChildElement( "gpx" );
     if( !root )
     {
         std::cerr << "Error parsing gpx file" << std::endl;
         return 0;
     }
 
+    const TiXmlElement* track = root->FirstChildElement("trk");
+    if( !track)
+    {
+        std::cerr << "Error parsing gpx file" << std::endl;
+        return 0;
+    }
 
+    std::string name, cmt, desc;
+    // Get Name
+    const TiXmlElement* pname = track->FirstChildElement("name");
+    if( !pname )
+        name = "Not name";
+    else
+        name = pname->GetText();
+    // Get Cmt
+    const TiXmlElement* pcmt = track->FirstChildElement("cmt");
+    if( !pcmt )
+        cmt = "Not cmt";
+    else
+        cmt = pcmt->GetText();
+    // Get Desc
+    const TiXmlElement* pdesc = track->FirstChildElement("desc");
+    if( !pdesc )
+        desc = "Not Description";
+    else
+        desc = pdesc->GetText();
+
+    const TiXmlElement* trackPoints = track->FirstChildElement("trkseg");
+    if( !trackPoints)
+    {
+        std::cerr << "Error parsing gpx file" << std::endl;
+        return 0;
+    }
+
+    TiXmlElement* point = (TiXmlElement*) trackPoints->FirstChildElement("trkpt");
+    unsigned npoints = 0;
+    while( point )
+    {
+        npoints++;
+        // Get Lat
+        const char* sLat  = point->Attribute("lat");
+        // Get Lon
+        const char* sLong = point->Attribute("lon");
+        // Get Ele
+        const TiXmlElement* pEle = point->FirstChildElement("ele");
+        const char* sElem = pEle ? pEle->GetText() : "";
+        // Get Time
+        const TiXmlElement* pTime = point->FirstChildElement("time");
+        const char* sTime = pTime ? pTime->GetText() : "";
+
+        point = point->NextSiblingElement("trkpt");
+    }
+
+    std::cout << "Number of points " << npoints << std::endl;
 }
