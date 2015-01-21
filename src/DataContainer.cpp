325c30f704794e4e2a25b7ac106de5b9db14c4e3
Carlos Duelo <carlos.duelo@gmail.com>
2015-01-21 17:38:42 +0100

    compute method

--- src/DataContainer.cpp
+++ src/DataContainer.cpp
@@ -1,6 +1,7 @@
 #include <DataContainer.h>
 
 #include <iostream>
+#include <math.h>
 
 namespace dgpx
 {
@@ -8,19 +9,43 @@ namespace dgpx
 void DataContainer::_insert( const TrackPoint& point)
 {
     if( point._ele > _maxEle )
+    {
         _maxEle = point._ele;
+        _maxEleIndex = _data.size();
+    }
     if( point._ele < _minEle )
+    {
         _minEle = point._ele;
+        _minEleIndex = _data.size();
+    }
 
     if( _data.size() > 0 )
     {
-        if( point._ele > _data.back()._ele )
-            _upHillEle += point._ele - _data.back()._ele; 
-        if( point._ele < _data.back()._ele )
-            _downHillEle += _data.back()._ele - point._ele; 
     }
 
     _data.push_back( point );
+}
+
+void DataContainer::compute()
+{
+
+    for( unsigned i = 0; i < _data.size(); i++ )
+    {
+        if( i < _data.size() - 1 )
+        {
+            float eleDiff = fabsf( _data[ i + 1 ]._ele - _data[i]._ele );
+            _avgEleDiff += eleDiff;
+            _maxEleDiff = fmaxf( _maxEleDiff, eleDiff );
+
+            if( _data[i]._ele > _data[i + 1 ]._ele )
+                _upHillEle += _data[i]._ele - _data[ i + 1 ]._ele;
+            if( _data[i]._ele < _data[ i + 1 ]._ele )
+                _downHillEle += _data[i + 1]._ele - _data[ i ]._ele;
+        }
+    }
+
+    _avgEleDiff /= (float) _data.size() - 1;
+
 }
 
 void DataContainer::printPoints()
@@ -31,11 +56,13 @@ void DataContainer::printPoints()
 
 void DataContainer::printStats()
 {
-    std::cout << "Stats:" << std::endl;
-    std::cout << "\t " << _maxEle << std::endl;
-    std::cout << "\t " << _minEle << std::endl;
-    std::cout << "\t " << _upHillEle << std::endl;
-    std::cout << "\t " << _downHillEle << std::endl;
+    std::cout << "Stats from GPX:" << std::endl;
+    std::cout << "\t Max Elevation: " << _maxEle << std::endl;
+    std::cout << "\t Min Elevation: " << _minEle << std::endl;
+    std::cout << "\t Elevation gain upHill: " << _upHillEle << std::endl;
+    std::cout << "\t Elevation gain downHill: " << _downHillEle << std::endl;
+    std::cout << "\t Max elevation difference: " << _maxEleDiff << std::endl;
+    std::cout << "\t Average elevation difference: " << _avgEleDiff << std::endl;
 }
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@ int main(int argc, char ** argv)
 
     dgpx::DataContainer data;
     parser.parse( data );
+    data.compute();
 
     data.printStats();
 }
